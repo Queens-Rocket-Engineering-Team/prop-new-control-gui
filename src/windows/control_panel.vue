6d0f4f3e2da020adbae1232300e632e2313ab93f
XZeeman2212 <xanderzeeman12@gmail.com>
2026-02-04 14:59:12 -0500

    Create background diagram image

--- src/windows/control_panel.vue
+++ src/windows/control_panel.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>Control Panel</div>
+    <div>Control Panel</div> <br>
+    <img :src="imageUrl"/>
 </template>
+
+<script setup>  
+const imageUrl = new URL('@assets/diagram.png', import.meta.url).href;
+</script>
